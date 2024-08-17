cb4d391ae02b46bb6b555144c1fe4120586ad101
FX <danielfx011@gmail.com>
2024-08-17 12:03:25 +0100

    fix

---
+++ exports/ai/Elevenlabs.js
@@ -0,0 +1,34 @@
+import { ElevenLabsClient } from 'elevenlabs'
+import { createWriteStream } from 'fs'
+import { v4 as uuid } from 'uuid'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY
+
+const client = new ElevenLabsClient({
+  apiKey: ELEVENLABS_API_KEY,
+})
+
+export const createAudioFileFromText = async (text) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const audio = await client.generate({
+        voice: 'Rachel',
+        model_id: 'eleven_turbo_v2_5',
+        text,
+      })
+      const fileName = `${uuid()}.mp3`
+      const fileStream = createWriteStream(fileName)
+
+      audio.pipe(fileStream)
+      fileStream.on('finish', () => resolve(fileName)) // Resolve with the fileName
+      fileStream.on('error', reject)
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
+
+await createAudioFileFromText('This is James')
