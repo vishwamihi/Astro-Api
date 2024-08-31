263c8f9d998004df1a527fb27825ef78c979b4eb
FX <danielfx011@gmail.com>
2024-08-31 19:14:53 +0100

    fix spotify

--- exports/download/spotify.js
+++ exports/download/spotify.js
@@ -1,12 +1,17 @@
 import fetch from 'node-fetch'
 
-const API_BASE_URL = 'https://api.guruapi.tech/spotifydl?url='
+const API_BASE_URL = 'https://ironman.koyeb.app/ironman/dl/spotify?link='
 
 export async function fetchSpotifyData(urlQuery) {
   try {
-    const response = await fetch(API_BASE_URL + urlQuery)
+    const encodedUrlQuery = encodeURIComponent(urlQuery)
+    const response = await fetch(API_BASE_URL + encodedUrlQuery)
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
     const responseData = await response.json()
-    return responseData.data
+    const { metadata, link } = responseData
+    return { metadata, link }
   } catch (error) {
     console.error('Error fetching Spotify data:', error)
     throw error
